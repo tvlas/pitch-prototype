c86558b60a10eb992ee3ce9c1cfb37f793b9afc0
tvlas <thiago.gilford@gmail.com>
2022-04-17 01:23:44 -0300

    Ball movement script

--- piche/Assets/Scripts/BallMovementBehavior.cs
+++ piche/Assets/Scripts/BallMovementBehavior.cs
@@ -2,17 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovementBehavior : MonoBehaviour
+public class BallMovementBehavior : MonoBehaviour
 {
-    
+    public float power = 10f;
+    public Rigidbody2D rb;
+    public Vector2 minPower;
+    public Vector2 maxPower;
+
+    Camera cam;
+    Vector2 force;
+    Vector3 startPoint;
+    Vector3 endPoint;
     void Start()
     {
-        
+        cam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Right click trigged");
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("Right click released");
+        }
     }
 }
